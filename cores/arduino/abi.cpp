1d49d4c2ee19f24fc18e67ca361dcfc9db7febf8
Martino Facchin <m.facchin@arduino.cc>
2024-12-18 10:48:36 +0100

    linker: forcefully remove exceptions and pack sections
    There's no way to convince the compiler to consider the provided flags
    when linking modern c++ code (std::).
    Get rid of the useless symbols in the linker and add the (then) missing
    implementations in abi.cpp

--- cores/arduino/abi.cpp
+++ cores/arduino/abi.cpp
@@ -6,19 +6,21 @@
 
 #include <stdlib.h>
 
-extern "C" void __cxa_pure_virtual(void) __attribute__ ((__noreturn__));
-extern "C" void __cxa_deleted_virtual(void) __attribute__ ((__noreturn__));
+extern "C" void __cxa_pure_virtual(void) {}
+extern "C" void __cxa_deleted_virtual(void) {}
+extern "C" int __cxa_atexit(void (*func) (void *), void * arg, void * dso_handle) {
+  return 0;
+}
 
 namespace std {
-  [[gnu::weak, noreturn]] void terminate() {
-    abort();
+  void __throw_length_error(const char* __s __attribute__((unused))) {}
+};
+
+extern "C" int strcmp(const char* s1, const char* s2) {
+  while(*s1 && (*s1 == *s2))
+  {
+    s1++;
+    s2++;
   }
-}
-
-void __cxa_pure_virtual(void) {
-  std::terminate();
-}
-
-void __cxa_deleted_virtual(void) {
-  std::terminate();
+  return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }

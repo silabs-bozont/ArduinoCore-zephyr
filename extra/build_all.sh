553c5a5d505d4364ebf9400e82130bdee7a32db7
Martino Facchin <m.facchin@arduino.cc>
2025-02-10 17:27:58 +0100

    build_all: add c33

--- extra/build_all.sh
+++ extra/build_all.sh
@@ -8,3 +8,4 @@ set -e
 ./extra/build.sh ek_ra8d1 ek_ra8d1
 ./extra/build.sh frdm_mcxn947/mcxn947/cpu0 frdm_mcxn947_mcxn947_cpu0
 ./extra/build.sh frdm_rw612 frdm_rw612
+./extra/build.sh arduino_portenta_c33 arduino_portenta_c33

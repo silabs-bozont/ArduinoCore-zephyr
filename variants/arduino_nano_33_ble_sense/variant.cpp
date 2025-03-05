372b7f55c25acaf219d7f009260277f07fb34d58
Kurt Eckhardt <kurte@rockisland.com>
2025-03-05 11:28:36 -0800

    Add call to initVariant in main move ble sense to variant.cpp
    Added call to main.cpp to initVariant.
    Added a weak version in main.cpp.
    
    Then moved the code for initializing the enable sensor pin out of main.cpp
    into the initVariant that I added to variant.cpp for the specific board

--- variants/arduino_nano_33_ble_sense/variant.cpp
+++ variants/arduino_nano_33_ble_sense/variant.cpp
@@ -1,6 +1,23 @@
+#include "Arduino.h"
 #include <hal/nrf_power.h>
+#include <zephyr/dt-bindings/gpio/nordic-nrf-gpio.h>
 
 void _on_1200_bps() {
     nrf_power_gpregret_set(NRF_POWER, 0, 0xb0);
     NVIC_SystemReset();
+}
+
+void initVariant(void) {
+  static const struct gpio_dt_spec enable_sensors =
+    GPIO_DT_SPEC_GET(DT_PATH(zephyr_user), pin_enable_gpios);
+  if (gpio_is_ready_dt(&enable_sensors)) {
+      gpio_flags_t flags = enable_sensors.dt_flags | GPIO_OUTPUT_HIGH | GPIO_ACTIVE_HIGH | NRF_GPIO_DRIVE_H0H1;
+      Serial.println((uint32_t)flags, HEX);
+      
+      gpio_pin_configure(enable_sensors.port, enable_sensors.pin, flags);
+      gpio_pin_set(enable_sensors.port, enable_sensors.pin, HIGH);
+      //Serial.println("### Sensor pin enabled ###");
+
+      delay(500);
+  }
 }

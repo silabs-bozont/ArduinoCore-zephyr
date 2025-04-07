25744b53795415e692bf10ab69e5b5bc9ef721b1
Luca Burelli <l.burelli@arduino.cc>
2025-04-07 15:22:14 +0200

    github: create matrix build to test Blink on all variants
    Convert the single test "Blink on giga" to a matrix build so that every
    variant of the core is tested with Blink.

--- extra/build_all.sh
+++ extra/build_all.sh
@@ -20,20 +20,38 @@ while getopts "hfl" opt; do
 	esac
 done
 
+if [ ! -z "$GITHUB_STEP_SUMMARY" ] ; then
+	echo "### Variant build results:" >> "$GITHUB_STEP_SUMMARY"
+fi
+
 jq -cr '.[]' < ./extra/targets.json | while read -r item; do
 	board=$(jq -cr '.board // ""' <<< "$item")
 	args=$(jq -cr '.args // ""' <<< "$item")
 
 	variant=$(extra/get_variant_name.sh "$board" || echo "$board")
-	echo && echo
-	echo ${variant}
-	echo ${variant} | sed -e 's/./=/g'
+	if [ -z "$GITHUB_STEP_SUMMARY" ] ; then
+		echo && echo
+		echo ${variant}
+		echo ${variant} | sed -e 's/./=/g'
+	else
+		echo "::group::=== ${variant} ==="
+	fi
 
 	./extra/build.sh "$board" $args
 	result=$?
 
-	echo
-	echo "${variant} result: $result"
+	if [ -z "$GITHUB_STEP_SUMMARY" ] ; then
+		echo
+		echo "${variant} result: $result"
+	else
+		echo "::endgroup::"
+		if [ $result -eq 0 ] ; then
+			echo "- :white_check_mark: \`${variant}\`" >> "$GITHUB_STEP_SUMMARY"
+		else
+			echo "^^^$(echo ${variant} | sed -e 's/./^/g')^^  FAILED with $result!"
+			echo "- :x: \`${variant}\`" >> "$GITHUB_STEP_SUMMARY"
+		fi
+	fi
 	[ $result -ne 0 ] && ! $FORCE && exit $result
 done
 

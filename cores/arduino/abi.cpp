67251fb0e89233b43b1f1b010b532f5eb18fc402
Luca Burelli <l.burelli@arduino.cc>
2025-04-10 18:20:18 +0200

    cores/arduino: warning squash
    Silence a number of trivial compiler warnings in the Arduino core:
    - unused arguments
    - "constexpr const"
    - signed/unsigned mismatches
    
    Signed-off-by: Luca Burelli <l.burelli@arduino.cc>

--- cores/arduino/abi.cpp
+++ cores/arduino/abi.cpp
@@ -9,6 +9,7 @@
 extern "C" void __cxa_pure_virtual(void) {}
 extern "C" void __cxa_deleted_virtual(void) {}
 extern "C" int __cxa_atexit(void (*func) (void *), void * arg, void * dso_handle) {
+  (void)func; (void)arg; (void)dso_handle; // unused
   return 0;
 }
 

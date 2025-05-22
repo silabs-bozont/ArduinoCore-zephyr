de869982d1115e76df2813b1f7ca05ee13b6efcc
kurte <kurte@rockisland.com>
2025-05-22 11:06:36 -0700

    PortentaH7 - Stop the blue blink...
    This was caused by the default sketch running on the M4 core when no other sketch was loaded
    on the M4.
    
    This changes the config to not automatically start the M4 at boot and.
    
    It appears like that left the Blue LED on, so added an InitVariant implementation for the PortentaH7 that turns it off.

--- variants/arduino_portenta_h7_stm32h747xx_m7/variant.cpp
+++ variants/arduino_portenta_h7_stm32h747xx_m7/variant.cpp
@@ -1,8 +1,20 @@
 #include <zephyr/kernel.h>
+#include <zephyrInternal.h>
 
 void _on_1200_bps() {
     uint32_t tmp = (uint32_t) & (RTC->BKP0R);
     tmp += (RTC_BKP_DR0 * 4U);
     *(__IO uint32_t *)tmp = (uint32_t)0xDF59;
     NVIC_SystemReset();
+}
+
+void initVariant(void) {
+    // check the BLUE LED
+    /* Set led1 inactive since the Arduino bootloader leaves it active */
+    const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
+    if (!gpio_is_ready_dt(&led2)) {
+        return;
+    }
+
+    gpio_pin_configure_dt(&led2, GPIO_OUTPUT_INACTIVE);
 }

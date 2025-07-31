d7cb41b507c5c0188f12cb6dbea3ec1be43a1e23
Leonardo Cavagnis <l.cavagnis@arduino.cc>
2025-07-31 18:30:56 +0200

    ethernet lib: set mac address, log module fix

--- libraries/SocketWrapper/Ethernet.h
+++ libraries/SocketWrapper/Ethernet.h
@@ -5,64 +5,76 @@
 #if DT_HAS_COMPAT_STATUS_OKAY(ethernet_phy)
 
 enum EthernetLinkStatus {
-	Unknown,
-	LinkON,
-	LinkOFF
+  Unknown,
+  LinkON,
+  LinkOFF
 };
 
 enum EthernetHardwareStatus {
-	EthernetNoHardware,
-	EthernetOk
+  EthernetNoHardware,
+  EthernetOk
 };
 
-class EthernetClass : public NetworkInterface {
+class EthernetClass: public NetworkInterface
+{
 public:
-	EthernetClass() {
-	}
+	EthernetClass() {}
+	virtual ~EthernetClass() {}
 
-	virtual ~EthernetClass() {
-	}
+    int begin(bool blocking = true, uint32_t additional_event_mask = 0) {
+        hardwareStatus();
+        return NetworkInterface::begin(blocking, additional_event_mask);
+    }
 
-	EthernetLinkStatus linkStatus() {
-		hardwareStatus();
-		if (net_if_is_up(netif)) {
-			return LinkON;
-		} else {
-			return LinkOFF;
-		}
-	}
+    int begin(uint8_t *mac, unsigned long timeout = 60000, unsigned long responseTimeout = 4000) {
+        hardwareStatus();
+        if (mac != nullptr) {
+            NetworkInterface::setMACAddress(mac);
+        }
+        return NetworkInterface::begin(true, 0);
+    }
 
-	bool begin(bool blocking = true, uint32_t additional_event_mask = 0) {
-		hardwareStatus();
-		return NetworkInterface::begin(blocking, additional_event_mask);
-	}
+	int maintain(); //TODO
+	
+    EthernetLinkStatus linkStatus() {
+        hardwareStatus();
+        if (net_if_is_up(netif)) {
+            return LinkON;
+        } else {
+            return LinkOFF;
+        }
+    }
+	
+    EthernetHardwareStatus hardwareStatus() {
+        const struct device *const dev = DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(ethernet_phy));
+        if (device_is_ready(dev)) {
+            for (int i = 1; i < 3; i++) {
+                auto _if = net_if_get_by_index(i);
+                if (!net_eth_type_is_wifi(_if)) {
+                    netif = _if;
+                    break;
+                }
+            }
+            return EthernetOk;
+        } else {
+            return EthernetNoHardware;
+        }
+    }    
 
-	bool begin(uint8_t *mac_address, int _timeout, int _response_timeout) {
-		return begin();
-	}
-
-	bool begin(uint8_t *mac_address, IPAddress _ip, IPAddress _dns, IPAddress _gateway,
-			   IPAddress _netmask, int _timeout, int _response_timeout) {
-		return begin();
-	}
-
-	EthernetHardwareStatus hardwareStatus() {
-		const struct device *const dev = DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(ethernet_phy));
-		if (device_is_ready(dev)) {
-			for (int i = 1; i < 4; i++) {
-				auto _if = net_if_get_by_index(i);
-				if (_if && !net_eth_type_is_wifi(_if)) {
-					netif = _if;
-					break;
-				}
-			}
-			return EthernetOk;
-		} else {
-			return EthernetNoHardware;
-		}
-	}
+    int begin(uint8_t *mac, IPAddress ip) {
+        return begin(); //TODO
+    }
+	int begin(uint8_t *mac, IPAddress ip, IPAddress dns) {
+        return begin(); //TODO
+    }
+	int begin(uint8_t *mac, IPAddress ip, IPAddress dns, IPAddress gateway) {
+        return begin(); //TODO
+    }
+	int begin(uint8_t *mac, IPAddress ip, IPAddress dns, IPAddress gateway, IPAddress subnet) {
+        return begin(); //TODO
+    }  
+	void init(uint8_t sspin = 10); //TODO
 };
-
 extern EthernetClass Ethernet;
 
 #endif

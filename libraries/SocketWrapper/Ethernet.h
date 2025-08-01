ba46a9cff203e894bbd0a354c0f0533eb6b3e534
pennam <m.pennasilico@arduino.cc>
2025-08-01 10:13:18 +0200

    Ethernet: add nullptr check on returned network interface

--- libraries/SocketWrapper/Ethernet.h
+++ libraries/SocketWrapper/Ethernet.h
@@ -48,7 +48,7 @@ public:
         if (device_is_ready(dev)) {
             for (int i = 1; i < 4; i++) {
                 auto _if = net_if_get_by_index(i);
-                if (!net_eth_type_is_wifi(_if)) {
+                if (_if && !net_eth_type_is_wifi(_if)) {
                     netif = _if;
                     break;
                 }

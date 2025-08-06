05660803192545f64885e0c67fe96b8ec69097e6
Leonardo Cavagnis <l.cavagnis@arduino.cc>
2025-08-06 16:51:50 +0200

    eth lib: add new line at end of file

--- libraries/SocketWrapper/ZephyrServer.h
+++ libraries/SocketWrapper/ZephyrServer.h
@@ -7,56 +7,50 @@
 
 class ZephyrServer : public arduino::Server, ZephyrSocketWrapper {
 private:
-	int _port;
+    int _port;
 
 public:
-	ZephyrServer() : _port(80) {};
-	ZephyrServer(uint16_t port) : _port(port) {};
+    ZephyrServer() : _port(80){};
+    ZephyrServer(uint16_t port) : _port(port){};
 
-	virtual ~ZephyrServer() {
-		end();
-	}
+    virtual ~ZephyrServer() {
+        end();
+    }
+    void end() {
+        ZephyrSocketWrapper::close();
+    }
+    void begin(uint16_t port) {
+        _port = port;
+        begin();
+    }
+    void begin() {
+        ZephyrSocketWrapper::bind(_port);
+        ZephyrSocketWrapper::listen(5);
+    }
+    uint8_t status() {
+        return 0;
+    }
+    explicit operator bool() {
+        return sock_fd != -1;
+    }
 
-	void end() {
-		ZephyrSocketWrapper::close();
-	}
+    ZephyrClient accept(uint8_t* status  = nullptr) {
+        ZephyrClient client;
+        int sock = ZephyrSocketWrapper::accept();
+        client.setSocket(sock);
+        return client;
+    }
 
-	void begin(uint16_t port) {
-		_port = port;
-		begin();
-	}
+    ZephyrClient available(uint8_t* status = nullptr) __attribute__((deprecated("Use accept()."))) {
+        return accept(status);
+    }
 
-	void begin() {
-		ZephyrSocketWrapper::bind(_port);
-		ZephyrSocketWrapper::listen(5);
-	}
+    size_t write(uint8_t c) override {
+        return write(&c, 1);
+    }
+    size_t write(const uint8_t* buffer, size_t size) override {
+        return send(buffer, size);
+    }
 
-	uint8_t status() {
-		return 0;
-	}
-
-	explicit operator bool() {
-		return sock_fd != -1;
-	}
-
-	ZephyrClient accept(uint8_t *status = nullptr) {
-		ZephyrClient client;
-		int sock = ZephyrSocketWrapper::accept();
-		client.setSocket(sock);
-		return client;
-	}
-
-	ZephyrClient available(uint8_t *status = nullptr) __attribute__((deprecated("Use accept()."))) {
-		return accept(status);
-	}
-
-	size_t write(uint8_t c) override {
-		return write(&c, 1);
-	}
-
-	size_t write(const uint8_t *buffer, size_t size) override {
-		return send(buffer, size);
-	}
-
-	friend class ZephyrClient;
+    friend class ZephyrClient;
 };

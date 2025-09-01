6df8dc2c9c4f4f33cc4c96144a26b5170ebfaa72
Tamas Jozsi <tamas.jozsi@silabs.com>
2025-09-01 10:37:09 +0200

    nano_matter: add initial support for the Arduino Nano Matter

--- cores/arduino/threads.cpp
+++ cores/arduino/threads.cpp
@@ -11,11 +11,14 @@ void start_static_threads() {
 #define _FOREACH_STATIC_THREAD(thread_data)                                                        \
 	STRUCT_SECTION_FOREACH (_static_thread_data, thread_data)
 
+	k_timeout_t timeout;
+	timeout.ticks = 0u;
+
 	_FOREACH_STATIC_THREAD(thread_data) {
 		k_thread_create(thread_data->init_thread, thread_data->init_stack,
 						thread_data->init_stack_size, thread_data->init_entry, thread_data->init_p1,
 						thread_data->init_p2, thread_data->init_p3, thread_data->init_prio,
-						thread_data->init_options, thread_data->init_delay);
+						thread_data->init_options, timeout);
 		k_thread_name_set(thread_data->init_thread, thread_data->init_name);
 		thread_data->init_thread->init_data = thread_data;
 	}

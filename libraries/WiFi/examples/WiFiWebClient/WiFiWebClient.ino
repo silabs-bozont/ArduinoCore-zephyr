347d4527f4ea88ed959657ba45bc527ab358ff88
pennam <m.pennasilico@arduino.cc>
2025-10-13 17:25:22 +0200

    WiFi: move library to a dedicated folder

--- libraries/WiFi/examples/WiFiWebClient/WiFiWebClient.ino
+++ libraries/WiFi/examples/WiFiWebClient/WiFiWebClient.ino
@@ -17,15 +17,15 @@
 
 #include "arduino_secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;        // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;                 // your network key Index number (needed only for WEP)
+char ssid[] = SECRET_SSID;  // your network SSID (name)
+char pass[] = SECRET_PASS;  // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;           // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 // IPAddress server(93,184,216,34);  // IP address for example.com (no DNS)
-char server[] = "example.com";       // host name for example.com (using DNS)
+char server[] = "example.com";  // host name for example.com (using DNS)
 
 ZephyrClient client;
 
@@ -33,14 +33,15 @@ void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ;  // wait for serial port to connect. Needed for native USB port only
   }
 
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("Communication with WiFi module failed!");
     // don't continue
-    while (true);
+    while (true)
+      ;
   }
 
   // attempt to connect to Wifi network:
@@ -49,7 +50,6 @@ void setup() {
     Serial.println(ssid);
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
-    Serial.println(status);
     // wait 3 seconds for connection:
     delay(3000);
   }
@@ -84,7 +84,8 @@ void loop() {
     client.stop();
 
     // do nothing forevermore:
-    while (true);
+    while (true)
+      ;
   }
 }
 
